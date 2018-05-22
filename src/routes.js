fae127e595fba677037a5d0b9735ce32cc7fb88c
Mac Read <james.macgregor.read@gmail.com>
2018-05-22 15:35:41 -0600

    Step 3

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom;'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 
 export default (
-    <Switch>
-        <Route component={ Home } exact Path='/'>
-        <Route component= { About } path='/about'>
+  <Switch>
+    <Route component={ Home } exact path="/" />
+    <Route component={ About } path="/about" />
+  </Switch>
 )
